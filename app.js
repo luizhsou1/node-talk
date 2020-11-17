fc5343173dc68c02739cd161f8fc08e1c0cdd0d2
Luiz Henrique <luiz.souza@awscode.com.br>
2020-11-17 13:29:37 -0300

    finalizado cadastro de contatos

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const path = require('path');
 const consign = require('consign');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const methodOverride = require('method-override');
+
+const error = require('./middlewares/error');
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cookieParser('node-talk'));
+app.use(expressSession());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 consign({})
   .include('models')
   .then('controllers')
   .then('routes')
-  .into(app);
+  .into(app)
+  ;
+
+// middleware de tratamento erros
+app.use(error.notFound);
+app.use(error.serverError);
 
 app.listen(3000, () => {
   console.log('Node Talk executando...');
