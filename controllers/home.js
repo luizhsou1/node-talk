2e0ee002d1532b9f8159a007ba2e7570106d5f8b
Luiz Henrique <luiz.souza@awscode.com.br>
2020-11-17 13:31:53 -0300

    terminado crud de contatos em memória do projeto

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,24 @@
 module.exports = (app) => {
   const HomeController = {
-    index: function(req, res) {
+    index(req, res) {
       res.render('home/index');
+    },
+
+    login(req, res) {
+      const { usuario } = req.body;
+      const { email, nome } = usuario;
+      if (email && nome) {
+        usuario.contatos = [];
+        req.session.usuario = usuario;
+        res.redirect('/contatos');
+      } else {
+        res.redirect('/');
+      }
+    },
+    
+    logout(req, res) {
+      req.session.destroy();
+      res.redirect('/');
     }
   };
   return HomeController;
